3e95c07e565444e538975e2225436b0892f026a7
Kapil Shanbhag <shanbhag.kapil09@gmail.com>
2022-05-09 17:21:38 +0530

    Initial Commit

---
+++ Assignment6/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Assignment6
+//
+//  Created by Kapil Ganesh Shanbhag on 09/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
